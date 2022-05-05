baece079d6a114f70a8cb4b65fa54126825a481c
gongqingkui T14 <gongqingkui@126.com>
2022-05-05 16:46:21 +0800

    refactor:py2 to py3
    改造Python2 代码为Python 3代码

--- names/config.py
+++ names/config.py
@@ -1,24 +1,23 @@
-﻿#encoding:utf-8
-import traceback
+﻿import traceback
 import pprint
 import re
 
 first_name = '王'
 gender = 'M'
-birthday='20170516120000'
-birth_p='山东'
-birth_c='滕州'
+birthday = '20170516120000'
+birth_p = '山东'
+birth_c = '滕州'
 
 
 def convert(ch):
     length = len('汉') #测试汉字占用字节数，utf-8，汉字占用3字节.bg2312，汉字占用2字节
     intord = ord(ch[0:1])
     if (intord >= 48 and intord <= 57):
-         return ch[0:1]
+        return ch[0:1]
     if (intord >= 65 and intord <=90 ) or (intord >= 97 and intord <=122):
         return ch[0:1].lower()
     ch = ch[0:length] #多个汉字只获取第一个
-    with open(r'pinying.txt') as f:
+    with open(r'pinying.txt','r',encoding='utf-8') as f:
         for line in f:
             if ch in line:
                  return line[len(line)-2:len(line)]
@@ -31,10 +30,10 @@ def score(x,m):
 
     try:
         r = requests.post(url,data=values) 
-        print r.text
+        print(r.text)
         #rs = re.findall('<font color=ff0000 size=5>(.+)</font>',r.text.encode('utf-8'))
         rs = re.findall('font (.+)font',r.text)
         return rs[0]
     except Exception as e:
         traceback.print_exc()
-        print e
+        print(e)
--- names/names.py
+++ names/names.py
@@ -1,29 +1,26 @@
-#encoding:utf-8
-
 from config import * 
 
-print '%s%s,%s,%s,%s'%(gender,first_name,birthday,birth_p,birth_c)
+print('%s%s,%s,%s,%s'%(gender,first_name,birthday,birth_p,birth_c))
 first_n_tone = convert(first_name)
 
 if gender == 'M':
-    f = open('boy_name.txt')
+    f = open('boy_name.txt',encoding='utf-8')
 elif gender == 'F':
-    f = open('girl_name.txt')
-f = open('girl_name.txt')
+    f = open('girl_name.txt',encoding='utf-8')
 for l in f:
     if l[0]=='-':
-        print l
+        print(l)
     elif len(l)>3: 
          middle_n_tone = convert(l[0:3])
          last_n_tone = convert(l[3:len(l)-1])
          
          if middle_n_tone != first_n_tone and last_n_tone != middle_n_tone : 
-             print first_name,l[:-1],score(first_name.decode('utf-8'),l.decode('utf-8'))
+             print(first_name,l[:-1],score(first_name.decode('utf-8'),l.decode('utf-8')))
          else:
              middle_n_tone = convert(l[0:3])
              if middle_n_tone != first_n_tone : 
                  try:
-                     print first_name,l[:len(l)-1],score(first_name,l)
+                     print(first_name,l[:len(l)-1],score(first_name,l))
                  except Exception as e:
-                     print e 
+                     print(e)
                      continue
